4584d545f43a9ad986aa62b96d90558303585392
Daniel Różycki <daniel.rozycki@stxnext.pl>
2024-11-26 08:43:24 +0100

    :bug: Fix updating DNS records

--- src/cloudflare_dns_updater/main.py
+++ src/cloudflare_dns_updater/main.py
@@ -1,6 +1,8 @@
 import asyncio
 from typing import List
 
+from loguru import logger
+
 from cloudflare_dns_updater.commands.dns import DNSUpdateCommand
 from cloudflare_dns_updater.queries import DeviceIPQuery
 from cloudflare_dns_updater.queries.dns import DNSRecordsQuery
@@ -10,6 +12,7 @@ from cloudflare_dns_updater.services.dns.value_objects import ZoneID
 
 async def main(skip: List[str]) -> None:
     # https://discuss.python.org/t/is-there-a-way-to-define-a-function-that-can-be-used-as-a-nomal-function-and-an-async-function/5262/5
+    logger.info("Running script, skipping: {}", skip)
     zone_id = ZoneID("457aba5ea6227a7f4d672e8845a2d5e9")
     device_ip, dns_records = await asyncio.gather(
         *[
@@ -17,15 +20,12 @@ async def main(skip: List[str]) -> None:
             DNSRecordsQuery.execute(zone_id=zone_id, skip=skip),
         ]
     )
-    print(dns_records)
     dns_records_to_update: List[UpdateDNSRecordDto] = []
-    # for dns_record in dns_records:  # type: DNSSingleRecord
-    #     update_dns_record_dto = UpdateDNSRecordDto(
-    #         content=device_ip,
-    #         id=dns_record.id,
-    #         name=dns_record.name,
-    #         type=dns_record.type,
-    #         zone_id=dns_record.zone_id,
-    #     )
-    #     dns_records_to_update.append(update_dns_record_dto)
+    logger.info("Records to update: {}", [r.name for r in dns_records])
+    for dns_record in dns_records:
+        dns_records_to_update.append(
+            UpdateDNSRecordDto.from_dns_single_record(
+                device_ip=device_ip, record=dns_record
+            )
+        )
     await DNSUpdateCommand.execute(dtos=dns_records_to_update)
--- src/cloudflare_dns_updater/services/dns/dtos/update.py
+++ src/cloudflare_dns_updater/services/dns/dtos/update.py
@@ -1,8 +1,12 @@
+from __future__ import annotations
+
 from uuid import UUID
 
 import attr
 
 from cloudflare_dns_updater.services.dns.constants import ZoneType
+from cloudflare_dns_updater.services.dns.value_objects import DNSSingleRecord
+from cloudflare_dns_updater.services.ip.value_objects import IP
 
 
 @attr.s(frozen=True, auto_attribs=True)
@@ -14,3 +18,15 @@ class UpdateDNSRecordDto:
     zone_id: UUID
     ttl: int = 1
     proxied: bool = True
+
+    @classmethod
+    def from_dns_single_record(
+        cls, device_ip: IP, record: DNSSingleRecord
+    ) -> UpdateDNSRecordDto:
+        return cls(
+            content=device_ip,
+            id=record.id,
+            name=record.name,
+            type=record.type,
+            zone_id=record.zone_id,
+        )
